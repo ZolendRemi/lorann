b5bbb52a49f77739599d2f8272de552326a46190
pierr <pierr@10.59.66.107>
2016-06-14 10:11:24 +0200

    test1 controller commentaire

--- JPU-BlankProject/controller/src/main/java/controller/Controller.java
+++ JPU-BlankProject/controller/src/main/java/controller/Controller.java
@@ -9,7 +9,7 @@ import contract.IView;
 /**
  * The Class Controller.
  */
-public class Controller implements IController { //test commit
+public class Controller implements IController { //test commit1
 
 	/** The view. */
 	private IView		view;
